de0b4e81f21cd893d5ee39daa7edd20dd929dab2
Nick Reid <nickreid@nickreid.com>
2012-03-12 09:04:48 -0700

    added page nav to group

--- functions-groups.php
+++ functions-groups.php
@@ -51,6 +51,21 @@ function sfhiv_add_group_events_list(){
 	<?	endif;
 }
 
+add_action('get_sidebar','sfhiv_add_parent_page_sidebar',20);
+function sfhiv_add_parent_page_sidebar(){
+	if(get_post_type()=='group' || get_post_type()=='event'){
+		$parent_query = new WP_Query( array(
+			'connected_type' => 'parent_page',
+			'connected_items' => get_the_ID(),
+		));
+		while($parent_query->have_posts()){
+			$parent_query->the_post();
+			sfhiv_draw_page_navigation(get_the_ID());
+		}
+		wp_reset_postdata();
+	}
+}
+
 add_action('get_sidebar','sfhiv_add_mini_archive_group_sidebar',25);
 function sfhiv_add_mini_archive_group_sidebar(){
 	if(is_page()){
