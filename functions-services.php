8ed886ec0066b9660e743406a853a02c7a3847c8
Nick Reid <nickreid@nickreid.com>
2012-03-12 18:18:53 -0700

    added structure for hours of operation meta box

--- functions-services.php
+++ functions-services.php
@@ -12,9 +12,18 @@ function sfhiv_add_services_type(){
 		'has_archive' => true,
 		'hierarchical' => true,
 		'taxonomies' => array('catagories'),
+		'register_meta_box_cb' => 'sfhiv_add_services_meta_boxes',
 		)
 	);
 	add_post_type_support( 'service', 'excerpt' );
+}
+
+function sfhiv_add_services_meta_boxes(){
+	add_meta_box( 'sfhiv_services_hours', 'Hours of Operation', 'sfhiv_services_hours_op_meta', 'service', 'normal' );
+}
+
+function sfhiv_services_hours_op_meta($post){
+	
 }
 
 ?>
