8ab6aab92d80a0015a4b3b02ba96003e6cc495c2
Nick Reid <nickreid@nickreid.com>
2012-03-14 11:07:04 -0700

    adding type styles

---
+++ content-event.php
@@ -0,0 +1,12 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+	</header><!-- .entry-header -->
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'toolbox' ), 'after' => '</div>' ) ); ?>
+	</div><!-- .entry-content -->
+	<footer class="entry-meta">
+		<?php edit_post_link( __( 'Edit', 'toolbox' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-meta -->
+</article><!-- #post-<?php the_ID(); ?> -->
