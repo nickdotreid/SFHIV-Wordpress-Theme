b8b168df69c49e63eac5dec67bf87352142a5e26
Nick Reid <nickreid@nickreid.com>
2012-03-16 08:14:27 -0700

    getting rid of parent connection for now

--- functions.php
+++ functions.php
@@ -34,12 +34,6 @@ function sfhiv_connection_types() {
 		),
 		'to' => 'user'
 	) );
-	
-	p2p_register_connection_type( array(
-		'name' => 'parent_page',
-		'from' => array('group','event'),
-		'to' => 'page',
-	) );
 }
 add_action( 'wp_loaded', 'sfhiv_connection_types' );
 
