b5beca89f78dca24d43b76c18d304921bdad02a4
Nick Reid <nickreid@nickreid.com>
2012-03-16 08:47:37 -0700

    changed group sidebar list to be more functional

--- functions-groups.php
+++ functions-groups.php
@@ -106,19 +106,21 @@ function sfhiv_add_mini_archive_group_sidebar(){
 		if($archive_type){
 			$query = mini_archive_get_query(get_the_ID());
 			$groups = sfhiv_get_groups_in($query);
-			$query = new WP_Query( array( 'post__in' => $groups, 'post_type'=>'sfhiv_group' ) );
-			if($query->post_count > 0):
-				?><nav><ul class="menu groups filters"><?
-				if($query->post_count > 1){
-					?><li class="" slug=""><a href="" slug="">All Groups</a></li><?
-				}
-				while($query->have_posts()):
-					$query->the_post();
-					get_template_part('menu-item','sfhiv_group');
-				endwhile;
-				?></ul></nav><?
-			endif;
-			wp_reset_postdata();
+			if(count($groups)>0){
+				$query = new WP_Query( array( 'post__in' => $groups, 'post_type'=>'sfhiv_group' ) );
+				if($query->post_count > 0):
+					?><nav><ul class="menu groups filters"><?
+					if($query->post_count > 1){
+						?><li class="" slug=""><a href="" slug="">All Groups</a></li><?
+					}
+					while($query->have_posts()):
+						$query->the_post();
+						get_template_part('menu-item','sfhiv_group');
+					endwhile;
+					?></ul></nav><?
+				endif;
+				wp_reset_postdata();				
+			}
 		}
 	}
 }
