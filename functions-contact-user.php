8fa96dcc9efb3c2840414ac86feb26e49ad276e1
Nick Reid <nickreid@nickreid.com>
2012-03-16 13:36:14 -0700

    added expanded profile

--- functions-contact-user.php
+++ functions-contact-user.php
@@ -1,6 +1,6 @@
 <?php
 
-add_action('get_sidebar','sfhiv_get_contact_user',5);
+add_action('get_sidebar','sfhiv_get_contact_user',105);
 function sfhiv_get_contact_user(){
 	if(in_array(get_post_type(),array('event','group','page','post'))):
 	$users = get_users( array(
--- list-member.php
+++ list-member.php
@@ -1,4 +1,13 @@
 <article class="member">
 	<span class="avatar"><?	userphoto_thumbnail($user);	?></span>
-	<span class="name"><?=get_the_author_meta('display_name',$user->ID);?></span>
+	<span class="info name"><?=get_the_author_meta('display_name',$user->ID);?></span>
+	<?	if(p2p_get_meta( $user->p2p_id, 'subject', true )):	?>
+	<span class="info title"><?=get_the_author_meta('title',$user->ID);?></span>
+	<?	else:	?>
+	<span class="info title"><?=get_the_author_meta('title',$user->ID);?></span>
+	<?	endif;	?>
+	<span class="info phone_number"><?=get_the_author_meta('phone_number',$user->ID);?></span>
+	<?	if(get_the_author_meta('email',$user->ID)):	?>
+	<span class="info email"><span class="label">Email:</span> <a href="mailto:<?=get_the_author_meta('email',$user->ID);?>"><?=get_the_author_meta('email',$user->ID);?></a></span>
+	<?	endif;	?>
 </article>
