d557dde30e381e2c0e28338a49aa94900c15b5d2
Nick Reid <nickreid@nickreid.com>
2012-03-16 10:40:22 -0700

    changed mini_archive to archive, so that loops and archive will be more generic

--- archive.php
+++ archive.php
@@ -1,7 +1,7 @@
-<aside id="mini_archive" class="list">
+<section id="archive" class="list">
 	<?	while($query->have_posts()):	$query->the_post();?>
 	
 	<?	get_template_part( 'list', $archive_type );	?>
 	
 	<?	endwhile;	?>
-</aside><!-- #mini_archive -->
+</section><!-- #mini_archive -->
--- loop.php
+++ loop.php
@@ -2,19 +2,8 @@
 			<?php if ( have_posts() ) : ?>
 
 				<?php toolbox_content_nav( 'nav-above' ); ?>
-
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-					?>
-
-				<?php endwhile; ?>
+				
+				<?php get_template_part( 'archive', get_post_format() );	?>
 
 				<?php toolbox_content_nav( 'nav-below' ); ?>
 
