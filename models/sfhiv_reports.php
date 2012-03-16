a867c3146ce14037fe4e8387f0ce78bdad75c8a0
Nick Reid <nickreid@nickreid.com>
2012-03-16 08:04:38 -0700

    moved report_categories to models

--- models/sfhiv_reports.php
+++ models/sfhiv_reports.php
@@ -21,5 +21,25 @@ function sfhiv_add_reports_type(){
 	);
 }
 
+add_action( 'init', 'sfhiv_create_report_categories' );
+function sfhiv_create_report_categories() {
+ $labels = array(
+    'name' => _x( 'Report Categories', 'taxonomy general name' ),
+    'singular_name' => _x( 'Report Category', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Report Categories' ),
+    'all_items' => __( 'All Report Categories' ),
+    'parent_item' => __( 'Parent Report Category' ),
+    'parent_item_colon' => __( 'Parent Report Category:' ),
+    'edit_item' => __( 'Edit Report Category' ),
+    'update_item' => __( 'Update Report Category' ),
+    'add_new_item' => __( 'Add New Report Category' ),
+    'new_item_name' => __( 'New Group Report Name' ),
+  ); 	
+
+  register_taxonomy('sfhiv_eport_category',array('sfhiv_report'),array(
+    'hierarchical' => true,
+    'labels' => $labels,
+  ));
+
 
 ?>
