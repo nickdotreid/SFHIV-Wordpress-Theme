f3fed1ae0f6215f65ce146be3d94b37b7c715ea0
Nick Reid <nickreid@nickreid.com>
2012-03-16 09:24:28 -0700

    abstracted get_years_in function to get_taxonomies_in

--- functions-years.php
+++ functions-years.php
@@ -9,51 +9,62 @@ function sfhiv_years_add_scripts(){
 
 add_action('get_sidebar','sfhiv_add_mini_archive_years_sidebar',20);
 function sfhiv_add_mini_archive_years_sidebar(){
+	$query = false;
 	if(is_page()){
 		$archive_type = mini_archive_on_page(get_the_ID());
 		if($archive_type){
 			$query = mini_archive_get_query(get_the_ID());
-			$years = sfhiv_get_years_in($query,'ids');
-			if(count($years)>0):
-				?><nav>
-					<ul class="menu filters years"><?
-					if(count($years)>1):
-						?><li class="menu-item"><a href="#" class="js-only filter default">All Years</a></li><?
-					endif;
-			wp_list_categories(array(
-				'taxonomy' => 'year',
-				'include' => implode(",",$years),
-				'title_li' => false,
-			));
-				?></ul></nav><?
-			endif;
 		}
 	}
+	if(is_archive()){
+		// get wp_query
+	}
+	if(!$query): return; endif;
+	$years = sfhiv_get_taxonomy_in($query,'sfhiv_years','ids');
+	if(count($years)>0):
+		?><nav>
+			<ul class="menu filters years"><?
+			if(count($years)>1):
+				?><li class="menu-item"><a href="#" class="js-only filter default">All Years</a></li><?
+			endif;
+	wp_list_categories(array(
+		'taxonomy' => 'year',
+		'include' => implode(",",$years),
+		'title_li' => false,
+	));
+		?></ul></nav><?
+	endif;
+	
 }
 
-function sfhiv_get_years_in($query,$fields=false){
-	$years = array();
+function sfhiv_get_taxonomy_in($query,$taxonomy_name,$fields=false){
+	$taxonomies = array();
 	$args = array();
 	if($fields){
 		$args['fields'] = $fields;
 	}
 	foreach($query->posts as $post){
-		$post_years = wp_get_object_terms($post->ID,'year',$args);
-		foreach($post_years as $year){
-			if(!in_array($year,$years)){
-				array_push($years,$year);
+		$post_taxonomies = wp_get_object_terms($post->ID,$taxonomy_name,$args);
+		foreach($post_taxonomies as $taxonomy){
+			if(!in_array($taxonomy,$taxonomies)){
+				array_push($taxonomies,$taxonomy);
 			}
 		}
 	}
+	foreach($taxonomies as $taxonomy){	// loop all entries to error check
+		if(isset($taxonomy['invalid_taxonomy'])){
+			return array();
+		}
+	}
 	if(!$fields){
-		usort($years,function($a,$b){
+		usort($taxonomies,function($a,$b){
 			if($a->term_order > $b->term_order){
 				return 1;
 			}
 			return 0;
 		});
 	}
-	return $years;
+	return $taxonomies;
 }
 
 
