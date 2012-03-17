4f2d43c6223903314a40385c00a06cfce68de026
Nick Reid <nickreid@nickreid.com>
2012-03-16 18:37:01 -0700

    styled contact member

--- contact-member.php
+++ contact-member.php
@@ -1,4 +1,4 @@
-<aside class="contact member">
+<aside class="contact list">
 	<p><?=p2p_get_meta( $user->p2p_id, 'subject', true );?></p>
 	<?	include(locate_template('list-member.php'));	?>
 </aside>
