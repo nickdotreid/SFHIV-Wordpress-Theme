f13c4ff556f891667a9705a3852dab41637c57ec
Nick Reid <nickreid@nickreid.com>
2012-03-17 18:34:48 -0700

    added document category

--- models/sfhiv_documents.php
+++ models/sfhiv_documents.php
@@ -17,11 +17,33 @@ function sfhiv_add_documents_type(){
 		),
 		'capability_type' => 'post',
 		'supports' => array('title','editor','excerpt'),
-		'taxonomies' => array('service_category'),
+		'taxonomies' => array(
+			'service_category',
+			'sfhiv_document_category',
+			),
 		)
 	);
 }
 
+add_action('init','sfhiv_add_document_category');
+function sfhiv_add_document_category(){
+	$labels = array(
+    'name' => _x( 'Document Categories', 'taxonomy general name' ),
+    'singular_name' => _x( 'Document Category', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Document Categories' ),
+    'all_items' => __( 'All Document Categories' ),
+    'parent_item' => __( 'Parent Document Category' ),
+    'parent_item_colon' => __( 'Parent Document Category:' ),
+    'edit_item' => __( 'Edit Document Category' ),
+    'update_item' => __( 'Update Document Category' ),
+    'add_new_item' => __( 'Add New Document Category' ),
+    'new_item_name' => __( 'New Group Document Name' ),
+  ); 	
 
+  register_taxonomy('sfhiv_document_category',array('sfhiv_document'),array(
+    'hierarchical' => true,
+    'labels' => $labels,
+  ));
+}
 
 ?>
