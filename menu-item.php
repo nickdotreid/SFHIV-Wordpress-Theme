e27b0d86670d3198198976aa7a3eacb3a902b650
Nick Reid <nickreid@nickreid.com>
2012-03-17 22:28:53 -0700

    styled menu items

--- menu-item.php
+++ menu-item.php
@@ -1,3 +1,3 @@
-<li class="<?	if(get_the_ID() == $post->ID) echo 'current-page-item';	?>">
+<li class="<?	if(get_the_ID() == $post->ID) echo 'current_item';	?>">
 	<a href="<?the_permalink($post->ID);?>" class="menu-item" group-id="<?the_ID($post->ID);?>" slug="<?=$post->post_name;?>" ><?=$post->post_title;?></a>
 </li>
