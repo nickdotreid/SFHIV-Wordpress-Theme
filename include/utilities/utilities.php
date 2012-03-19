86dad449c39d1768955796af16380784bbf43162
Nick Reid <nickreid@nickreid.com>
2012-03-18 19:12:11 -0700

    added function to strip get queryies from wp_queryies if applicable

--- include/utilities/utilities.php
+++ include/utilities/utilities.php
@@ -20,6 +20,29 @@ function sfhiv_get_archive_query(){
 		$wp_query->rewind_posts();
 		$query = $wp_query;
 	}
+	return sfhiv_remove_url_vars_from_query($query);
+}
+
+function sfhiv_remove_url_vars_from_query($query){
+	$replace = false;
+	$new_args = $query->query_vars;
+	$new_args['tax_query'] = array(
+		'relation' => $query->query_vars['tax_query']['relation'],
+	);
+	$url_keys = array_keys($_GET);
+	
+	foreach($query->query_vars['tax_query'] as $tax_query){
+		if(in_array($tax_query['taxonomy'],$url_keys) &&
+			($tax_query['terms'] == $_GET[$tax_query['taxonomy']])){
+			$replace = true;
+		}else{
+			array_push($new_args,$tax_query);
+		}
+	}
+	
+	if($replace){
+		return new WP_Query($new_args);
+	}
 	return $query;
 }
 
