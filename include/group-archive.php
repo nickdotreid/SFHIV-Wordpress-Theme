65f043169f8cb6bdc842b11a98f51ef1b6761751
Nick Reid <nickreid@nickreid.com>
2012-03-18 18:29:25 -0700

    made gorup query not show up on group pages

--- include/group-archive.php
+++ include/group-archive.php
@@ -1,25 +1,25 @@
 <?php
 
+add_action('get_sidebar','sfhiv_group_archive_filter_by_years',22);
+function sfhiv_group_archive_filter_by_years(){
+	$query = sfhiv_get_archive_query();
+	if(!$query || $query->query_vars['post_type'] != 'sfhiv_group') return;
+	$years = sfhiv_get_taxonomy_in($query,'sfhiv_year','ids');
+	if(count($years)<2) return;
+	sfhiv_draw_taxonomy_filter(array(
+		'taxonomy' => 'sfhiv_year',
+		'include' => implode(",",$years),
+		'title_li' => false,
+	));
+}
+
 add_action('get_sidebar','sfhiv_groups_in_query_sidebar',25);
 function sfhiv_groups_in_query_sidebar(){
 	$query = sfhiv_get_archive_query();
-	if(!$query): return; endif;
+	if(!$query || $query->query_vars['post_type'] == 'sfhiv_group'): return; endif;
 	$groups = sfhiv_get_related_in($query,'group_events');
-	if(count($groups)>0){
-		$query = new WP_Query( array( 'post__in' => $groups, 'post_type'=>'sfhiv_group' ) );
-		if($query->post_count > 0):
-			?><nav><ul class="menu groups filters"><?
-			if($query->post_count > 1){
-				?><li class="" slug=""><a href="" slug="">All Groups</a></li><?
-			}
-			while($query->have_posts()):
-				$query->the_post();
-				get_template_part('menu-item','sfhiv_group');
-			endwhile;
-			?></ul></nav><?
-		endif;
-		wp_reset_postdata();				
-	}
+	if(count($groups)<2) return;
+	sfhiv_draw_menu($groups);
 }
 
 add_action('get_sidebar','sfhiv_add_query_group_cat_sidebar',21);
