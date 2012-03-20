495d10afed8686f1b71443aaa17f5ccbb8fc7c97
Nick Reid <nickreid@nickreid.com>
2012-03-20 11:30:28 -0700

    added link to list type

--- list.php
+++ list.php
@@ -1,6 +1,6 @@
 <article id="post-<?=the_ID();?>" <?php post_class("list-item"); ?>>
 	<header>
-		<h1><?=the_title();?></h1>
+		<h1 class="entry-title"><a href="<?=the_permalink();?>"><?=the_title();?></a></h1>
 	</header>
 	<div class="entry-content">
 		<?=the_excerpt();?>
