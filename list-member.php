feca258ce6c06a49e585bb7fa892cb03406b86a6
Nick Reid <nickreid@nickreid.com>
2012-03-22 15:31:17 -0700

    added user custom fields to list

--- list-member.php
+++ list-member.php
@@ -3,9 +3,15 @@
 	<span class="info name"><?=get_the_author_meta('display_name',$user->ID);?></span>
 	<?	if(p2p_get_meta( $user->p2p_id, 'title', true )):	?>
 	<span class="info title"><?=p2p_get_meta( $user->p2p_id, 'title', true );?></span>
+	<?	elseif(get_the_author_meta('sfhiv_title',$user->ID)): ?>
+	<span class="info title"><?=get_the_author_meta('sfhiv_title',$user->ID);?></span>
 	<?	endif;	?>
-	<span class="info title"><?=get_the_author_meta('title',$user->ID);?></span>
-	<span class="info phone_number"><?=get_the_author_meta('phone_number',$user->ID);?></span>
+	<?	if(get_the_author_meta('sfhiv_department',$user->ID)):	?>
+	<span class="info department"><?=get_the_author_meta('sfhiv_department',$user->ID);?></span>
+	<?	endif;	?>
+	<?	if(get_the_author_meta('sfhiv_telephone',$user->ID)):	?>
+	<span class="info phone"><span class="label">Phone:</span> <?=get_the_author_meta('sfhiv_telephone',$user->ID);?></span>
+	<?	endif;	?>
 	<?	if(get_the_author_meta('email',$user->ID)):	?>
 	<span class="info email"><span class="label">Email:</span> <a href="mailto:<?=get_the_author_meta('email',$user->ID);?>"><?=get_the_author_meta('email',$user->ID);?></a></span>
 	<?	endif;	?>
