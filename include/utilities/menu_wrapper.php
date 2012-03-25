9d0325a85aed03d1f84908f5aa7c25398a4c463a
Nick Reid <nickreid@nickreid.com>
2012-03-25 14:04:10 -0700

    moved draw page nav function to file with other menu funcitons

--- include/utilities/menu_wrapper.php
+++ include/utilities/menu_wrapper.php
@@ -1,5 +1,43 @@
 <?php
 
+
+function sfhiv_draw_page_navigation($ID){
+	?><nav><?
+	$children = get_pages(array(
+		"parent"=>$ID,
+		"hierarchical" => 0,
+		"post_type" => get_post_type($ID),
+		));
+	$children_IDs = array();
+	foreach($children as $child){
+		array_push($children_IDs,$child->ID);
+	}
+	$children_IDs = implode(",",$children_IDs);
+	$parents = get_ancestors($ID,get_post_type($ID));
+	$parent_IDs = implode(",",$parents);
+	$page_IDs = $parent_IDs.",".$children_IDs.",".$ID;
+	if(count($parents)>0){
+		$parent_ID = $parents[0];
+		$sibling_IDs = array();
+		$siblings = get_pages(array(
+			"parent"=>$parent_ID,
+			"hierarchical" => 0,
+			"post_type" => get_post_type($ID),
+		));
+		foreach($siblings as $sibling){
+			array_push($sibling_IDs,$sibling->ID);
+		}
+		$page_IDs .= ",".implode(",",$sibling_IDs);
+	}
+	wp_page_menu(array( 
+		'show_home' => false,
+		'sort_column' => 'menu_order',
+		'include' => $page_IDs,
+		'post_type' => get_post_type($ID),
+		));
+	?></nav><?
+}
+
 function sfhiv_draw_menu($posts=array()){
 	$menu_templates = array('menu.php');
 	$template = locate_template($menu_templates);
