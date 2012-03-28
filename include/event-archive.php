2cc4435e0e678eda87e8055f4613d76a058e26b3
Nick Reid <nickreid@nickreid.com>
2012-03-28 13:57:15 -0700

    added sidebars to events

--- include/event-archive.php
+++ include/event-archive.php
@@ -15,4 +15,20 @@ function sfhiv_event_filter($query=false,$args){
 	));
 }
 
+add_action('get_sidebar','sfhiv_event_archive_event_categories',22);
+function sfhiv_event_archive_event_categories(){
+	$query = sfhiv_get_archive_query();
+	if(!$query || $query->query_vars['post_type'] != 'sfhiv_event') return;
+	sfhiv_draw_taxonomy_query_menu('sfhiv_event_category',$query);
+}
+
+add_action('get_sidebar','sfhiv_event_archive_related_groups',25);
+function sfhiv_event_archive_related_groups(){
+	$query = sfhiv_get_archive_query();
+	if(!$query || $query->query_vars['post_type'] == 'sfhiv_event'): return; endif;
+	$groups = sfhiv_get_related_in($query,'group_events');
+	if(count($groups)<2) return;
+	sfhiv_draw_menu($groups);
+}
+
 ?>
