f27b8e7eac374925e6e7b1711888b98778bc5b76
Nick Reid <nickreid@nickreid.com>
2012-03-30 11:14:10 -0700

    added link to attachemt

--- list-attachment.php
+++ list-attachment.php
@@ -1,11 +1,12 @@
 <article id="post-<?=the_ID();?>" <?php post_class("list-item mime-".sanitize_title(get_post_mime_type())); ?>>
+	<a class="icon-link" href="<?=wp_get_attachment_url();?>">Download <?=the_title();?></a>
 	<header>
-		<h1 class="entry-title"><a href="<?=the_permalink();?>"><?=the_title();?></a></h1>
+		<h1 class="entry-title"><a href="<?=wp_get_attachment_url();?>"><?=the_title();?></a></h1>
 	</header>
 	<div class="entry-content">
 		<?=the_excerpt();?>
 	</div>
 	<nav>
-		<a href="<?=wp_get_attachment_url();?>">Download</a>
+		<a href="<?=wp_get_attachment_url();?>">Download <?=the_title();?></a>
 	</nav>
 </article>
