1db55520c1ccb1d0d65c681a73e94206b836aed5
Nick Reid <nickreid@nickreid.com>
2012-03-30 10:19:26 -0700

    added in foo bar service location

--- list-sfhiv_service_hour.php
+++ list-sfhiv_service_hour.php
@@ -12,13 +12,7 @@ $time_format = get_option('time_format');
 <article id="post-<?=the_ID();?>" <?php post_class("list-item sfhiv_service"); ?>
 	service-parent="<?=$service_parent->ID;?>"
 	>
-	<header>
-		<h1 class="entry-title"><a href="<?=get_permalink($service_parent->ID);?>"><?=get_the_title($service_parent->ID);?></a></h1>
-	</header>
-	<div class="entry-content">
-		<?=apply_filters('the_excerpt',$service_parent->post_excerpt);?>
-	</div>
-	<aside class="day_time column">
+	<aside class="day_time column left">
 		<? $days = get_post_meta(get_the_ID(),"sfhiv_service_days");?>
 		<?	if(count($days)>0):	?>
 		<div class="days">
@@ -34,7 +28,13 @@ $time_format = get_option('time_format');
 			<span class="end"><span class="label">End:</span><?=date($time_format,get_post_meta(get_the_ID(),"sfhiv_service_end",true));?></span>
 		</div>
 	</aside>
-	<aside class="relationships column">
+	<aside class="location">
+		<h3>Title title</h3>
+		<span class="address line">25 Van Ness Ave</span>
+		<span class="address line">San Francisco, CA 94602</span>
+		<span class="address line hint">HINT HINT HINT</span>
+	</aside>
+	<aside class="relationships column right">
 		<? $service_groups = sfhiv_service_get_groups($service_parent->ID);	?>
 		<?	if( count($service_groups) > 0):	?>
 		<ul class="related_groups">
@@ -64,6 +64,12 @@ $time_format = get_option('time_format');
 		</ul>
 		<?	endif;	?>
 	</aside>
+	<header>
+		<h1 class="entry-title"><a href="<?=get_permalink($service_parent->ID);?>"><?=get_the_title($service_parent->ID);?></a></h1>
+	</header>
+	<div class="entry-content">
+		<?=apply_filters('the_excerpt',$service_parent->post_excerpt);?>
+	</div>
 	<nav>
 		<a href="<?=get_permalink($service_parent->ID);?>"><?=__('View '.get_the_title($service_parent->ID),'sfhiv_theme');?></a>
 		<?php edit_post_link( __( 'Edit', 'sfhiv_theme' ), '<span class="sep"> | </span><span class="edit-link">', '</span>', $service_parent->ID ); ?>
