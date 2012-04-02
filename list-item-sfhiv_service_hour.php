7c7046edb95ef2a5262b51a59c01e60b6a52511a
Nick Reid <nickreid@nickreid.com>
2012-04-02 10:02:21 -0700

    added location to second item

--- list-item-sfhiv_service_hour.php
+++ list-item-sfhiv_service_hour.php
@@ -8,38 +8,44 @@ if($service_parents->post_count > 0)	$service_parent = $service_parents->posts[0
 
 $time_format = get_option('time_format');
 
+$location = sfhiv_location_get_related_location(get_the_ID());
 ?>
 <article id="post-<?=the_ID();?>" <?php post_class("list-item sfhiv_service"); ?>
 	service-parent="<?=$service_parent->ID;?>"
 	>
-	<aside class="day_time column">
-		<? $days = get_post_meta(get_the_ID(),"sfhiv_service_days");?>
-		<?	if(count($days)>0):	?>
-		<div class="days">
-			<ul>
-			<?	foreach($days as $day):	?>
-			<li class="day"><?=$day;?></li>
-			<?	endforeach;	?>
-			</ul>
-		</div>
+	<section class="group">
+		<aside class="day_time">
+			<? $days = get_post_meta(get_the_ID(),"sfhiv_service_days");?>
+			<?	if(count($days)>0):	?>
+			<div class="days">
+				<ul>
+				<?	foreach($days as $day):	?>
+				<li class="day"><?=$day;?></li>
+				<?	endforeach;	?>
+				</ul>
+			</div>
+			<?	endif;	?>
+			<div class="time">
+				<span class="start"><span class="label">Start:</span><?=date($time_format,get_post_meta(get_the_ID(),"sfhiv_service_start",true));?></span>
+				<span class="end"><span class="label">End:</span><?=date($time_format,get_post_meta(get_the_ID(),"sfhiv_service_end",true));?></span>
+			</div>
+		</aside>
+		<?	if($location):	?>
+		<aside class="location">
+			<h3><?=apply_filters("the_title",$location->post_title);?></h3>
+			<span class="address line"><?=get_post_meta($location->ID,'sfhiv_room',true);?></span>
+			<span class="address line"><?=get_post_meta($location->ID,'sfhiv_address',true);?></span>
+			<?	$city = get_post_meta($location->ID,'sfhiv_city',true);?>
+			<?	if($city != 'San Francisco'):	?>
+			<span class="address line"><?=get_post_meta($location->ID,'sfhiv_city',true);?>, <?=get_post_meta($location->ID,'sfhiv_state',true);?> <?=get_post_meta($location->ID,'sfhiv_zip_code',true);?></span>
+			<?	endif;	?>
+			<!-- View on Google Maps Link here -->
+			<span class="address line hint"><?apply_filters('the_content',get_post_meta($location->ID,'sfhiv_location_hint',true));?></span>
+		</aside>
 		<?	endif;	?>
-		<div class="time">
-			<span class="start"><span class="label">Start:</span><?=date($time_format,get_post_meta(get_the_ID(),"sfhiv_service_start",true));?></span>
-			<span class="end"><span class="label">End:</span><?=date($time_format,get_post_meta(get_the_ID(),"sfhiv_service_end",true));?></span>
-		</div>
-	</aside>
+		<br class="clear" />
+	</section>
 	<aside class="relationships column">
-		<? $service_groups = sfhiv_service_get_groups($service_parent->ID);	?>
-		<?	if( count($service_groups) > 0):	?>
-		<ul class="related_groups">
-		<?	foreach($service_groups as $sgroup):	?>
-			<li class="group <?=implode(" ",wp_get_object_terms($sgroup->ID,'sfhiv_group_category',array("fields"=>"slugs")));?>">
-				<?=get_the_title($sgroup->ID);?>
-			</li>
-		<?	endforeach;	?>
-		</ul>
-		<?	endif;	?>
-		<!-- LIST SERVICES -->
 		<?	$service_types = wp_get_object_terms($service_parent->ID,'sfhiv_service_category');	?>
 		<?	if(count($service_types)>0):	?>
 		<ul class="service_categories">
@@ -48,7 +54,6 @@ $time_format = get_option('time_format');
 		<?	endforeach;	?>
 		</ul>
 		<?	endif;	?>
-		<!-- LIST POPULATION -->
 		<?	$population_types = wp_get_object_terms($service_parent->ID,'sfhiv_population_category');	?>
 		<?	if(count($population_types)>0):	?>
 		<ul class="population_categories">
