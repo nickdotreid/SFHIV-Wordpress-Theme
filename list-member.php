03d3ffa387712b8c40c68146c2b21e967293e16d
Nick Reid <nickreid@nickreid.com>
2012-04-01 23:51:05 -0700

    styled members with contact info

--- list-member.php
+++ list-member.php
@@ -1,16 +1,18 @@
 <article class="member <?if($show_phone || $show_email) echo "has_contact";?>">
-	<span class="avatar"><?	userphoto_thumbnail($user);	?></span>
-	<span class="info name"><?=get_the_author_meta('display_name',$user->ID);?></span>
-	<?	if(p2p_get_meta( $user->p2p_id, 'title', true )):	?>
-	<span class="info title"><?=p2p_get_meta( $user->p2p_id, 'title', true );?></span>
-	<?	elseif(get_the_author_meta('sfhiv_title',$user->ID)): ?>
-	<span class="info title"><?=get_the_author_meta('sfhiv_title',$user->ID);?></span>
-	<?	endif;	?>
-	<?	if(get_the_author_meta('sfhiv_department',$user->ID)):	?>
-	<span class="info department"><?=get_the_author_meta('sfhiv_department',$user->ID);?></span>
-	<?	endif;	?>
+	<section class="profile">
+		<span class="avatar"><?	userphoto_thumbnail($user);	?></span>
+		<span class="info name"><?=get_the_author_meta('display_name',$user->ID);?></span>
+		<?	if(p2p_get_meta( $user->p2p_id, 'title', true )):	?>
+		<span class="info title"><?=p2p_get_meta( $user->p2p_id, 'title', true );?></span>
+		<?	elseif(get_the_author_meta('sfhiv_title',$user->ID)): ?>
+		<span class="info title"><?=get_the_author_meta('sfhiv_title',$user->ID);?></span>
+		<?	endif;	?>
+		<?	if(get_the_author_meta('sfhiv_department',$user->ID)):	?>
+		<span class="info department"><?=get_the_author_meta('sfhiv_department',$user->ID);?></span>
+		<?	endif;	?>
+	</section>
 	<?	if($show_phone || $show_email):	?>
-	<section class="contact info">
+	<section class="contact">
 		<?	if($show_phone && get_the_author_meta('sfhiv_telephone',$user->ID)):	?>
 		<span class="info phone"><span class="label">Phone:</span> <?=get_the_author_meta('sfhiv_telephone',$user->ID);?></span>
 		<?	endif;	?>
@@ -19,4 +21,5 @@
 		<?	endif;	?>
 	</section>
 	<?	endif;	?>
+	<br class="clear" />
 </article>
