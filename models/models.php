094052292b7a38b531634e7a4131f6979bb1227f
Nick Reid <nickreid@nickreid.com>
2012-04-04 09:57:04 -0700

    added sfhiv models type

--- models/models.php
+++ models/models.php
@@ -7,6 +7,7 @@ require_once('sfhiv_year_tag.php');
 // Register Post Types
 require_once('sfhiv_group.php');
 require_once('sfhiv_reports.php');
+require_once('sfhiv_studies.php');
 require_once('sfhiv_services.php');
 require_once('sfhiv_training.php');
 require_once('sfhiv_documents.php');
---
+++ models/sfhiv_studies.php
@@ -0,0 +1,46 @@
+<?php
+
+add_action('init','sfhiv_add_studies_type');
+function sfhiv_add_studies_type(){
+	register_post_type( 'sfhiv_study',
+		array(
+			'labels' => array(
+				'name' => __( 'Studies' ),
+				'singular_name' => __( 'Study' )
+			),
+		'public' => true,
+		'has_archive' => true,
+		'hierarchical' => true,
+		'rewrite' => array(
+			'slug' => 'studies',
+			'feeds' => true,
+		),
+		'capability_type' => 'page',
+		'supports' => array('title','editor','excerpt','thumbnail','page-attributes'),
+		)
+	);
+}
+
+add_action( 'init', 'sfhiv_create_study_categories' );
+function sfhiv_create_study_categories() {
+ $labels = array(
+    'name' => _x( 'Study Categories', 'taxonomy general name' ),
+    'singular_name' => _x( 'Study Category', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Study Categories' ),
+    'all_items' => __( 'All Study Categories' ),
+    'parent_item' => __( 'Parent Study Category' ),
+    'parent_item_colon' => __( 'Parent Study Category:' ),
+    'edit_item' => __( 'Edit Study Category' ),
+    'update_item' => __( 'Update Study Category' ),
+    'add_new_item' => __( 'Add New Study Category' ),
+    'new_item_name' => __( 'New Group Study Name' ),
+  ); 	
+
+  register_taxonomy('sfhiv_study_category',array('sfhiv_study'),array(
+    'hierarchical' => true,
+    'labels' => $labels,
+  ));
+}
+
+
+?>
