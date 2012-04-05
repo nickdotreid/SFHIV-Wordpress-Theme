2b2c847e6a7bad825ffbc41244b3a1bdc962a5d2
Nick Reid <nickreid@nickreid.com>
2012-04-05 09:40:19 -0700

    styled service hour

--- list-item-sfhiv_service_hour.php
+++ list-item-sfhiv_service_hour.php
@@ -13,24 +13,20 @@ $location = sfhiv_location_get_related_location(get_the_ID());
 <article id="post-<?=the_ID();?>" <?php post_class("list-item sfhiv_service"); ?>
 	service-parent="<?=$service_parent->ID;?>"
 	>
-	<section class="group">
-		<aside class="day_time">
-			<? $days = get_post_meta(get_the_ID(),"sfhiv_service_days");?>
-			<?	if(count($days)>0):	?>
-			<div class="days">
-				<ul>
-				<?	foreach($days as $day):	?>
-				<li class="day"><?=$day;?></li>
-				<?	endforeach;	?>
-				</ul>
-			</div>
-			<?	endif;	?>
-			<div class="time">
-				<span class="start"><span class="label">Start:</span><?=date($time_format,get_post_meta(get_the_ID(),"sfhiv_service_start",true));?></span>
-				<span class="end"><span class="label">End:</span><?=date($time_format,get_post_meta(get_the_ID(),"sfhiv_service_end",true));?></span>
-			</div>
-			<br class="clear" />
-		</aside>
+		<? $days = get_post_meta(get_the_ID(),"sfhiv_service_days");?>
+		<?	if(count($days)>0):	?>
+		<div class="days">
+			<ul>
+			<?	foreach($days as $day):	?>
+			<li class="day"><?=$day;?></li>
+			<?	endforeach;	?>
+			</ul>
+		</div>
+		<?	endif;	?>
+		<div class="time">
+			<span class="start"><span class="label">Start:</span><?=date($time_format,get_post_meta(get_the_ID(),"sfhiv_service_start",true));?></span>
+			<span class="end"><span class="label">End:</span><?=date($time_format,get_post_meta(get_the_ID(),"sfhiv_service_end",true));?></span>
+		</div>
 		<?	if($location):	?>
 		<aside class="location">
 			<h3><?=apply_filters("the_title",$location->post_title);?></h3>
@@ -44,24 +40,29 @@ $location = sfhiv_location_get_related_location(get_the_ID());
 			<span class="address line hint"><?apply_filters('the_content',get_post_meta($location->ID,'sfhiv_location_hint',true));?></span>
 		</aside>
 		<?	endif;	?>
-		<br class="clear" />
-	</section>
-	<aside class="relationships column">
 		<?	$service_types = wp_get_object_terms($service_parent->ID,'sfhiv_service_category');	?>
 		<?	if(count($service_types)>0):	?>
-		<ul class="service_categories">
-		<?	foreach($service_types as $category):	?>
-			<li class="category <?=$category->taxonomy;?> <?=$category->slug;?>"><?=$category->name;?></li>
-		<?	endforeach;	?>
-		</ul>
+		<aside class="category-list">
+			<h3>Services Offered</h3>
+			<ul class="service_categories">
+			<?	foreach($service_types as $category):	?>
+				<li class="category <?=$category->taxonomy;?> <?=$category->slug;?>"><?=$category->name;?></li>
+			<?	endforeach;	?>
+			</ul>
+			<br class="clear" />
+		</aside>
 		<?	endif;	?>
+
 		<?	$population_types = wp_get_object_terms($service_parent->ID,'sfhiv_population_category');	?>
 		<?	if(count($population_types)>0):	?>
+		<aside class="category-list">
+			<h3><?_e('Only Serves');?></h3>
 		<ul class="population_categories">
 		<?	foreach($population_types as $category):	?>
 			<li class="category <?=$category->taxonomy;?> <?=$category->slug;?>"><?=$category->name;?></li>
 		<?	endforeach;	?>
 		</ul>
+		<br class="clear" />
+		</aside>
 		<?	endif;	?>
-	</aside>
 </article>
