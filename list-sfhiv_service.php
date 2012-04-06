e68be692c2c1dc6cc8f58d8041abbc76eee7a0c8
Nick Reid <nickreid@nickreid.com>
2012-04-05 23:05:24 -0700

    moved service hour styles to service

--- list-sfhiv_service.php
+++ list-sfhiv_service.php
@@ -2,15 +2,42 @@
 	populations="<?=implode(",",wp_get_object_terms(get_the_ID(),'sfhiv_population_category',array("fields"=>"slugs")));?>"
 	services="<?=implode(",",wp_get_object_terms(get_the_ID(),'sfhiv_service_category',array("fields"=>"slugs")));?>"
 	>
-	<header>
-		<h1 class="entry-title"><a href="<?=the_permalink();?>"><?=the_title();?></a></h1>
-	</header>
-	<div class="entry-content column left">
-		<?=the_excerpt();?>
+	<div class="column left">
+		<header>
+			<h1 class="entry-title"><a href="<?=the_permalink();?>"><?=the_title();?></a></h1>
+		</header>
+		<div class="entry-content">
+			<?=the_excerpt();?>
+		</div>
+		<!-- LIST SERVICES -->
+		<?	$service_types = wp_get_object_terms(get_the_ID(),'sfhiv_service_category');	?>
+		<?	if(count($service_types)>0):	?>
+		<aside class="category-list">
+			<h3>Services Offered</h3>
+		<ul class="service_categories">
+		<?	foreach($service_types as $category):	?>
+			<li class="category <?=$category->taxonomy;?> <?=$category->slug;?>"><?=$category->name;?></li>
+		<?	endforeach;	?>
+		</ul>
+		</aside>
+		<?	endif;	?>
+		<!-- LIST POPULATION -->
+		<?	$population_types = wp_get_object_terms(get_the_ID(),'sfhiv_population_category');	?>
+		<?	if(count($population_types)>0):	?>
+		<aside class="category-list">
+			<h3>Population Specific</h3>
+		<ul class="population_categories">
+		<?	foreach($population_types as $category):	?>
+			<li class="category <?=$category->taxonomy;?> <?=$category->slug;?>"><?=$category->name;?></li>
+		<?	endforeach;	?>
+		</ul>
+		</aside>
+		<?	endif;	?>
 	</div>
-	<aside class="relationships column right">
+	<div class="column right">
 		<? $service_groups = sfhiv_service_get_groups();	?>
 		<?	if( count($service_groups) > 0):	?>
+		<aside class="related-groups">
 		<ul class="related_groups">
 		<?	foreach($service_groups as $sgroup):	?>
 			<li class="group <?=implode(" ",wp_get_object_terms($sgroup->ID,'sfhiv_group_category',array("fields"=>"slugs")));?>">
@@ -29,26 +56,9 @@
 			</li>
 		<?	endforeach;	?>
 		</ul>
+		</aside>
 		<?	endif;	?>
-		<!-- LIST SERVICES -->
-		<?	$service_types = wp_get_object_terms(get_the_ID(),'sfhiv_service_category');	?>
-		<?	if(count($service_types)>0):	?>
-		<ul class="service_categories">
-		<?	foreach($service_types as $category):	?>
-			<li class="category <?=$category->taxonomy;?> <?=$category->slug;?>"><?=$category->name;?></li>
-		<?	endforeach;	?>
-		</ul>
-		<?	endif;	?>
-		<!-- LIST POPULATION -->
-		<?	$population_types = wp_get_object_terms(get_the_ID(),'sfhiv_population_category');	?>
-		<?	if(count($population_types)>0):	?>
-		<ul class="population_categories">
-		<?	foreach($population_types as $category):	?>
-			<li class="category <?=$category->taxonomy;?> <?=$category->slug;?>"><?=$category->name;?></li>
-		<?	endforeach;	?>
-		</ul>
-		<?	endif;	?>
-	</aside>
+	</div>
 	<!-- LIST HOURS (if events should be separate) -->
 	<nav>
 		<a href="<?=the_permalink();?>"><?=__('View '.get_the_title(),'sfhiv_theme');?></a>
