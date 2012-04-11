fcd0f6e9f4076bafa3a43df3ed146bfc3cd4967f
Nick Reid <nickreid@nickreid.com>
2012-04-11 10:04:15 -0700

    added user ID and first name/last name to list

--- list-member.php
+++ list-member.php
@@ -1,7 +1,7 @@
-<article class="member <?if($show_phone || $show_email) echo "has_contact";?>">
+<article id="user-<?=$user->ID;?>" class="member <?if($show_phone || $show_email) echo "has_contact";?>">
 	<section class="profile">
 		<span class="avatar"><?	userphoto_thumbnail($user);	?></span>
-		<span class="info name"><?=get_the_author_meta('display_name',$user->ID);?></span>
+		<span class="info name"><?=get_the_author_meta('user_firstname',$user->ID);?> <?=get_the_author_meta('user_lastname',$user->ID);?></span>
 		<?	if(p2p_get_meta( $user->p2p_id, 'title', true )):	?>
 		<span class="info title"><?=p2p_get_meta( $user->p2p_id, 'title', true );?></span>
 		<?	elseif(get_the_author_meta('sfhiv_title',$user->ID)): ?>
