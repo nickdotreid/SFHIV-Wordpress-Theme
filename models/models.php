6f0287eba61c6087915478327d9704de7b21003e
Nick Reid <nickreid@nickreid.com>
2012-04-18 08:43:01 -0700

    removed sfhiv_reports and blended into sfhiv_documents

--- models/models.php
+++ models/models.php
@@ -6,7 +6,6 @@ require_once('sfhiv_year_tag.php');
 
 // Register Post Types
 require_once('sfhiv_group.php');
-require_once('sfhiv_reports.php');
 require_once('sfhiv_studies.php');
 require_once('sfhiv_services.php');
 require_once('sfhiv_training.php');
