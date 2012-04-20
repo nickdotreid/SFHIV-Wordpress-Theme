4b5e06c11656592dc716990e88d19caf3cae563a
Nick Reid <nickreid@nickreid.com>
2012-04-20 08:12:24 -0700

    removed extra number listing

--- models/related_pages/templates/related_page_item.php
+++ models/related_pages/templates/related_page_item.php
@@ -1,5 +1,4 @@
 <li id="sfhiv_related-<?=$item->ID;?>" class="sfhiv_related_page">
-	<?=p2p_get_meta($item->p2p_id,'order',true);?>
 	<input type="hidden" name="sfhiv_related[]" value="<?=$item->ID;?>" />
 	<a href="#" class="add">Add <?=apply_filters('the_title',$item->post_title);?></a>
 	<a href="#" class="remove">Remove <?=apply_filters('the_title',$item->post_title);?></a>
