0da079a1e1eb7a9b8f8e456a6ed22b48aa05e627
Nick Reid <nickreid@nickreid.com>
2012-04-23 08:51:45 -0700

    hide similar document menu

--- include/document-page.php
+++ include/document-page.php
@@ -27,7 +27,7 @@ function sfhiv_document_page_service_categories(){
 	));
 }
 
-add_action('get_sidebar','sfhiv_document_page_similar_menu',23);
+//add_action('get_sidebar','sfhiv_document_page_similar_menu',23);
 function sfhiv_document_page_similar_menu(){
 	if (!is_singular('sfhiv_document')) return;
 	$query = get_similar_to(get_post(get_the_ID()));
