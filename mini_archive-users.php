7e8bdcfb317912081ba0728fa60c2af2e36f81ee
Nick Reid <nickreid@nickreid.com>
2012-04-26 15:17:29 -0700

    made users sort in this context -- not really perfect, but...

--- mini_archive-users.php
+++ mini_archive-users.php
@@ -1,5 +1,20 @@
 <section id="members" class="list">
 	<?
+	usort($users,function($a,$b){
+		if(get_the_author_meta('user_lastname',$a->ID) > get_the_author_meta('user_lastname',$b->ID)){
+			return 1;
+		}
+		if(get_the_author_meta('user_lastname',$a->ID) < get_the_author_meta('user_lastname',$b->ID)){
+			return -1;
+		}
+		if(get_the_author_meta('user_firstname',$a->ID) > get_the_author_meta('user_firstname',$b->ID)){
+			return 1;
+		}
+		if(get_the_author_meta('user_firstname',$a->ID) < get_the_author_meta('user_firstname',$b->ID)){
+			return -1;
+		}
+		return 0;
+	});
 	foreach($users as $user):
 		include(locate_template('list-member.php'));
 	endforeach;
