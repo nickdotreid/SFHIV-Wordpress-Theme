9cd64087822a834beaf8ca765df80f1a33bdd41c
Nick Reid <nickreid@nickreid.com>
2012-04-27 14:15:25 -0700

    changed date filter functions

--- include/event-archive.php
+++ include/event-archive.php
@@ -10,7 +10,7 @@ function sfhiv_event_archive_loop_arguments($args,$post_type){
 add_action('sfhiv_pre_loop','sfhiv_event_filter',10,2);
 function sfhiv_event_filter($query=false,$args){
 	if(!$query || $query->query_vars['post_type'] != 'sfhiv_event') return;
-	sfhiv_draw_filters('sfhiv_event_time',array(
+	sfhiv_draw_filters('sfhiv_event_time',$query->sfhiv_event_selection,array(
 		array(
 			"value" => "upcoming",
 			"name" => "Upcoming",
--- include/utilities/filters.php
+++ include/utilities/filters.php
@@ -1,14 +1,14 @@
 <?php
 
-function sfhiv_draw_filters($filter_key,$options=array(),$all_text="All",$legend_text="",$button_text="Update"){
+function sfhiv_draw_filters($filter_key,$selected_value = false,$options=array(),$all_text="All"){
 		?>
 		<nav>
 			<ul class="filters menu">
 				<li class="cat-item "><a href="<?=remove_query_arg($filter_key);?>"><?_e($all_text);?></a></li>
 				<?	foreach($options as $option):	?>
 				<li class="cat-item <?
-					if($_GET[$filter_key] == $option['value'] ||
-							($option['default'] && !isset($_GET['filter_key']))) echo 'current-item"';
+					if($selected_value == $option['value'] ||
+							($option['default'] && !$selected_value)) echo 'current_item"';
 				?>">
 					<a href="<?=add_query_arg($filter_key,$option['value']);?>"><?=$option['name'];?></a>
 				</li>
