c3a010adc35faf05bb46179861cbc978bb127f9f
Nick Reid <nickreid@nickreid.com>
2012-04-30 15:24:22 -0700

    fixed attachment loop bug that screwed up loop context

--- include/common-list.php
+++ include/common-list.php
@@ -2,6 +2,7 @@
 
 add_action('short_after_content','sfhiv_list_list_attachments',5);
 function sfhiv_list_list_attachments(){
+	global $post;
 	$attachments = new WP_Query(array(
 		'post_status' => 'any',
 		'post_type' => 'attachment',
@@ -11,13 +12,15 @@ function sfhiv_list_list_attachments(){
 		'post_mime_type' => 'application'
 		) );
 	if($attachments->post_count < 1) return;
+	$original_post = get_post();
 	echo '<ul class="attachments">';
 	while($attachments->have_posts()){
 		$attachments->the_post();
 		get_template_part('list-item',get_post_type());
 	}
 	echo '</ul>';
-	wp_reset_postdata();
+	setup_postdata($post);
+	$post = $original_post;
 }
 
 ?>
