c177f741f0acd3a3398511e76c53317d33ea6954
Nick Reid <nickreid@nickreid.com>
2012-05-02 15:14:50 -0700

    made general list item type for all post types

--- list-item.php
+++ list-item.php
@@ -1,6 +1,10 @@
-<li id="post-<?=the_ID();?>" <?php post_class("list-item mime-".sanitize_title(get_post_mime_type())); ?>>
-	<a href="<?=wp_get_attachment_url();?>"><?=the_title();?></a>
-	<span class="description">
-		<span><?=get_post_mime_type();?></span>
-	</span>
+<li id="post-<?=the_ID();?>" <?php 
+	post_class("list-item");
+	do_action("list-item");
+	?>>
+	<a href="<?=the_permalink();?>">
+		<?	do_action('before_list-item');	?>
+		<?=the_title();?>
+		<?	do_action('after_list-item');	?>
+	</a>
 </li>
