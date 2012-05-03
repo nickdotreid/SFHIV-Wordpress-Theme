2da53b97d10f81166290d1b1195260f154e02fdb
Nick Reid <nickreid@nickreid.com>
2012-05-03 11:51:58 -0700

    made slider a little slicker

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,6 +1,50 @@
 $(document).ready(function(){
 	$('#featured').jcarousel({
 		'scroll': 1,
-		'wrap':'circular'
+		'wrap':'circular',
+		'auto':3,
+		initCallback: sfhiv_carousel_initCallback,
+		itemVisibleInCallback: sfhiv_carousel_visible,
+		itemVisibleOutCallback: {
+			onBeforeAnimation:sfhiv_carousel_hide
+		}
 	});
 });
+
+function sfhiv_carousel_hide(options,item,index,state){
+	item = $(item);
+	$("article",item).animate({
+		'margin-top':item.height()+'px'
+	},{
+		'duration':500
+	});
+}
+
+function sfhiv_carousel_visible(options,item,index,state){
+	item = $(item);
+	val = $(item).height() - $("article",item).height();
+	$("article",item).css('margin-top',item.height()+'px');
+	$("article",item).animate({
+		'margin-top':val+'px'
+	},{
+		'duration':500
+	});
+}
+
+function sfhiv_carousel_initCallback(carousel){
+    // Disable autoscrolling if the user clicks the prev or next button.
+    carousel.buttonNext.bind('click', function() {
+        carousel.startAuto(0);
+    });
+
+    carousel.buttonPrev.bind('click', function() {
+        carousel.startAuto(0);
+    });
+
+    // Pause autoscrolling if the user moves with the cursor over the clip.
+    carousel.clip.hover(function() {
+        carousel.stopAuto();
+    }, function() {
+        carousel.startAuto();
+    });
+};
