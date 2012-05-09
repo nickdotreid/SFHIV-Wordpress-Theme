a31f245542d57fc26e6531b63d677f73e29aca95
Nick Reid <nickreid@nickreid.com>
2012-05-09 07:56:02 -0700

    replaced longer column float fixing with shorter version

--- assets/js/page.js
+++ assets/js/page.js
@@ -1,45 +1,14 @@
 $(document).ready(function(){
-	$(".three-column,.two-column").bind("redraw",function(event){
-		var column = $(this);
-		var item_selector = ".preview, .widget";
-		// reset all heights to auto & restore all borders
-		$(item_selector,column).css({
+	$("#members,.three-column,.two-column").bind("redraw",function(){
+		var item_selector = ".preview, .widget, .member";
+
+		$(item_selector,$(this)).css({
 			"height":"auto"
 		});
-		var heights = [];
-		$(item_selector,column).each(function(){
-			var position = $(this).position();
-			var found = false;
-			for(var i=0;i<heights.length;i++){
-				if(heights[i]['top']==position.top){
-					found = true;
-					if($(this).height()>heights[i]['height']){
-						heights[i]['height']=$(this).height();
-					}
-				}
-			}
-			if(!found){
-				heights.push({
-					"top":position.top,
-					"height":$(this).height()
-				});
-			}
-		});
-		$(item_selector,column).each(function(){
-			var position = $(this).position();
-			for(var i=0;i<heights.length;i++){
-				if(heights[i]['top']==position.top){
-					$(this).height(heights[i]['height']);
-				}
-			}
-		});
-	});
-	
-	$("#members").bind("redraw",function(){
 		var _top = 0;
 		var _height = 0;
 		var _in_row = [];
-		$(".member",$(this)).each(function(){
+		$(item_selector,$(this)).each(function(){
 			member = $(this);
 			if(member.offset().top != _top){
 				_top = member.offset().top;
