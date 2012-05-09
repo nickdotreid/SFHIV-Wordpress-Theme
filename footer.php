377d6b2bc50d406287c26ea5e34bc0c06c48c0b1
Nick Reid <nickreid@nickreid.com>
2012-05-09 08:24:54 -0700

    put line break in between modified date & author name

--- footer.php
+++ footer.php
@@ -19,8 +19,8 @@
 			<section class="thin">
 				<?_e("Last updated on");?>
 				<?the_modified_date();?>
-				<?_e("by");?>
-				<?the_modified_author();?>
+				<br />
+				<?_e("by");?> <?the_modified_author();?>
 				<nav>
 				<? if ( is_user_logged_in() ): ?>
 				<a href="<?php echo wp_logout_url( get_permalink() ); ?>" title="Logout">Logout</a>
