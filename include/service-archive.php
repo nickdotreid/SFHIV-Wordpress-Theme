946e22ca6eac0c8d7e968962247cffa494c902ef
Nick Reid <nickreid@nickreid.com>
2012-05-18 08:27:07 -0700

    changed how filters work

--- include/service-archive.php
+++ include/service-archive.php
@@ -1,34 +1,24 @@
 <?php
 
-add_action('get_sidebar','sfhiv_service_archive_add_service_category_menu',21);
-function sfhiv_service_archive_add_service_category_menu(){
-	$query = sfhiv_get_archive_query();
+add_action('sfhiv_pre_loop','sfhiv_service_archive_add_service_category_menu',21,2);
+function sfhiv_service_archive_add_service_category_menu($query=false,$args=array()){
 	if(!$query || $query->query_vars['post_type'] != 'sfhiv_service') return;
-	sfhiv_draw_taxonomy_query_menu('sfhiv_service_category',$query);
+	sfhiv_draw_taxonomy_query_menu('sfhiv_service_category',$query,array(
+		'title_li' => false,
+		'all_taxonomy_name' => false,
+		'extra_classes' => 'filters',
+		'base_link' => $_SERVER['REQUEST_URI'],
+	));
 }
 
-add_action('get_sidebar','sfhiv_service_archive_add_population_category_menu',21);
-function sfhiv_service_archive_add_population_category_menu(){
-	$query = sfhiv_get_archive_query();
+add_action('sfhiv_pre_loop','sfhiv_service_archive_add_population_category_menu',21,2);
+function sfhiv_service_archive_add_population_category_menu($query=false,$args=array()){
 	if(!$query || $query->query_vars['post_type'] != 'sfhiv_service') return;
-	sfhiv_draw_taxonomy_query_menu('sfhiv_population_category',$query);
-}
-
-
-//add_action('get_sidebar','sfhiv_service_hour_archive_add_service_category_menu',21);
-function sfhiv_service_hour_archive_add_service_category_menu(){
-	$query = sfhiv_get_archive_query();
-	if(!$query || $query->query_vars['post_type'] != 'sfhiv_service_hour') return;
-	sfhiv_draw_taxonomy_query_menu('sfhiv_service_category',$query);
-}
-
-//add_action('get_sidebar','sfhiv_service_hour_archive_population_category_menu',22,2);
-function sfhiv_service_hour_archive_population_category_menu($query=false,$args=array()){
-	$query = sfhiv_get_archive_query();
-	if(!$query || $query->query_vars['post_type'] != 'sfhiv_service_hour') return;
 	sfhiv_draw_taxonomy_query_menu('sfhiv_population_category',$query,array(
-		'title_li' => 'Population Specific',
-		'base_link' => get_permalink(),
+		'title_li' => false,
+		'all_taxonomy_name' => 'Any Population',
+		'extra_classes' => 'filters',
+		'base_link' => $_SERVER['REQUEST_URI'],
 	));
 }
 
