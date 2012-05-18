74deffc91c359142777ee93c12506ca86b94b185
Nick Reid <nickreid@nickreid.com>
2012-05-18 14:49:00 -0700

    used contact fields to make user meta boxes

--- models/sfhiv_user.php
+++ models/sfhiv_user.php
@@ -5,6 +5,14 @@ function sfhiv_no_contact_info($contact_methods){
 	unset($contact_methods['aim']);
 	unset($contact_methods['yim']);
 	unset($contact_methods['jabber']);
+	
+	$contact_methods['sfhiv_suffix'] = 'Suffix';
+	$contact_methods['sfhiv_telephone'] = 'Telephone';
+	
+	$contact_methods['sfhiv_title'] = 'Title';
+	$contact_methods['sfhiv_department'] = 'Department';
+	
+	
 	return $contact_methods;
 }
 
@@ -59,80 +67,6 @@ function sfhiv_save_show_bio( $user_id ) {
 	}
 }
 
-add_action( 'show_user_profile', 'sfhiv_user_telephone' );
-add_action( 'edit_user_profile', 'sfhiv_user_telephone' );
-function sfhiv_user_telephone($user){
-	?>
-	<table class="form-table">
-		<tr>
-		<th><label for="sfhiv_telephone">Phone <span class="description">Use this one</span></label></th>
-		<td>
-			<input type="text" name="sfhiv_telephone" id="sfhiv_telephone" value="<?php echo esc_attr( get_the_author_meta( 'sfhiv_telephone', $user->ID ) ); ?>" class="regular-text" />
-		</td>
-		</tr>
-	</table>
-	<?
-}
-add_action( 'personal_options_update', 'sfhiv_save_user_telephone' );
-add_action( 'edit_user_profile_update', 'sfhiv_save_user_telephone' );
-function sfhiv_save_user_telephone( $user_id ) {
-	if ( !current_user_can( 'edit_user', $user_id ) )
-		return FALSE;
-	// should clean phone number and format
-	update_usermeta( $user_id, 'sfhiv_telephone', $_POST['sfhiv_telephone'] );
-}
-
-add_action( 'show_user_profile', 'sfhiv_user_suffix', 1 );
-add_action( 'edit_user_profile', 'sfhiv_user_suffix', 1 );
-function sfhiv_user_suffix($user){
-	?>
-	<table class="form-table">
-		<tr>
-		<th><label for="sfhiv_suffix">Suffix <span class="description">Displayed after last name</span></label></th>
-		<td>
-			<input type="text" name="sfhiv_suffix" id="sfhiv_suffix" value="<?php echo esc_attr( get_the_author_meta( 'sfhiv_suffix', $user->ID ) ); ?>" class="regular-text" />
-		</td>
-		</tr>
-	</table>
-	<?
-}
-add_action( 'personal_options_update', 'sfhiv_save_user_suffix' );
-add_action( 'edit_user_profile_update', 'sfhiv_save_user_suffix' );
-function sfhiv_save_user_suffix( $user_id ) {
-	if ( !current_user_can( 'edit_user', $user_id ) )
-		return FALSE;
-	update_usermeta( $user_id, 'sfhiv_suffix', $_POST['sfhiv_suffix'] );
-}
-
-
-add_action( 'show_user_profile', 'sfhiv_user_title' );
-add_action( 'edit_user_profile', 'sfhiv_user_title' );
-function sfhiv_user_title($user){
-	?>
-	<table class="form-table">
-		<tr>
-		<th><label for="sfhiv_title">Title <span class="description">Default title to display</span></label></th>
-		<td>
-			<input type="text" name="sfhiv_title" id="sfhiv_title" value="<?php echo esc_attr( get_the_author_meta( 'sfhiv_title', $user->ID ) ); ?>" class="regular-text" />
-		</td>
-		</tr>
-		<tr>
-		<th><label for="sfhiv_department">Department <span class="description">Name of department member belongs to</span></label></th>
-		<td>
-			<input type="text" name="sfhiv_department" id="sfhiv_department" value="<?php echo esc_attr( get_the_author_meta( 'sfhiv_department', $user->ID ) ); ?>" class="regular-text" />
-		</td>
-		</tr>
-	</table>
-	<?
-}
-add_action( 'personal_options_update', 'sfhiv_save_user_title' );
-add_action( 'edit_user_profile_update', 'sfhiv_save_user_title' );
-function sfhiv_save_user_title( $user_id ) {
-	if ( !current_user_can( 'edit_user', $user_id ) )
-		return FALSE;
-	update_usermeta( $user_id, 'sfhiv_title', $_POST['sfhiv_title'] );
-	update_usermeta( $user_id, 'sfhiv_department', $_POST['sfhiv_department'] );
-}
 
 function sfhiv_users_sort(&$users){
 	$users = apply_filters('sfhiv_users_sort',$users);
