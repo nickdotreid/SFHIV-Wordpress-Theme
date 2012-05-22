59e59fb2845ab8533dee59eaa5986f4beafee2e9
Nick Reid <nickreid@nickreid.com>
2012-05-22 08:51:17 -0700

    added javascript spacing function

--- assets/js/page.js
+++ assets/js/page.js
@@ -27,15 +27,18 @@ $(document).ready(function(){
 		$(this).height(member.outerHeight()+member.position().top);
 	});
 	
-	$("#access ul.menu").bind("redraw",function(){
+	$("#access").bind("redraw",function(){
 		var menu = $(this);
 		var free_space = menu.width();
 		var menu_items = $(".menu-item:not(.sub-menu .menu-item)",menu);
 		menu_items.each(function(){
-			var item = $(this);
-			free_space -= item.width();
+			free_space = free_space - $(this).width();
 		});
-		free_space = free_space/menu_items.length;
+		var num_items = menu_items.length-1;
+		if(num_items<1){
+			num_items = 1;
+		}
+		free_space = free_space/num_items;
 		menu_items.css("margin-right",free_space+'px');
 		$(".menu-item:not(.sub-menu .menu-item):last",menu).css("margin-right",'0px');
 	});
@@ -43,5 +46,7 @@ $(document).ready(function(){
 	$(window).resize(function(){
 		$(".three-column,.two-column,#members").trigger("redraw");
 		$(".list-item").trigger("redraw");
+		$("#access").trigger("redraw");
 	}).resize();
+	setTimeout('$(window).resize()',500);
 });
