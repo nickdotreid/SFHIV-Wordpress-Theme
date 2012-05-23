c48158746dc32ced77453c4b816c0e16dd8e35a6
Nick Reid <nickreid@nickreid.com>
2012-05-23 16:45:35 -0700

    made author meta one line and simpler

--- include/post-page.php
+++ include/post-page.php
@@ -6,21 +6,15 @@ function sfhiv_post_sidebar(){
 	dynamic_sidebar("Blog Sidebar");
 }
 
-add_action('after_content','sfhiv_post_author',7);
-function sfhiv_post_author(){
+add_action('before_content','sfhiv_post_meta',7);
+function sfhiv_post_meta(){
 	if(!is_singular("post")) return;
 	$user = get_user_by('login', get_the_author_meta('user_login'));
 	if(!$user) return;
-	echo '<div class="post-author">';
-		include(locate_template('contact-member.php'));
-	echo '</div>';
-}
-
-add_action('after_content','sfhiv_post_date',8);
-function sfhiv_post_date(){
-	if(!is_singular("post")) return;
-	echo '<div class="post-date">';
-	_e('Posted on: ','sfhiv_theme');
+	echo '<div class="post-meta">';
+	echo 'Posted by ';
+	echo get_the_author_meta("first_name",$user->ID).' '.get_the_author_meta("last_name",$user->ID);
+	echo ' on ';
 	echo get_the_date();
 	echo '</div>';
 }
