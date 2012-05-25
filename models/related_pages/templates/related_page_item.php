e47588b5b582f85d9314da64102a0962fad7a685
Nick Reid <nickreid@nickreid.com>
2012-05-25 15:40:54 -0700

    already moved models to seperate plugin

--- models/related_pages/templates/related_page_item.php
+++
@@ -1,7 +0,0 @@
-<li id="sfhiv_related-<?=$item->ID;?>" class="sfhiv_related_page">
-	<input type="hidden" name="sfhiv_related[]" value="<?=$item->ID;?>" />
-	<a href="#" class="add">Add <?=apply_filters('the_title',$item->post_title);?></a>
-	<a href="#" class="remove">Remove <?=apply_filters('the_title',$item->post_title);?></a>
-	<span class="title"><?=apply_filters('the_title',$item->post_title);?></span>
-	<span class="post_type"><?=get_post_type($item->ID);?></span>
-</li>
--- models/templates/service_hours.php
+++
@@ -1,8 +0,0 @@
-<fieldset>
-	<legend>Days of Week</legend>
-	<?	foreach($day_terms as $day):	?>
-	<label class="checkbox"><input type="checkbox" name="<?=$form_name;?>[days][]" value="<?=$day->slug;?>" <?	
-		if(in_array($day->slug,$days)){ echo 'checked="checked"'; }	
-			?>  /><?=$day->name;?></label>
-	<?	endforeach;	?>
-</fieldset>
