6c87055dbd7cde8d2f6ede9689fcf0748fc6d7b7
Nick Reid <nickreid@nickreid.com>
2012-05-31 11:37:29 -0700

    made years nav horizontal

--- include/group-archive.php
+++ include/group-archive.php
@@ -5,7 +5,7 @@ function sfhiv_group_archive_filter_by_years($query,$args){
 	if(!$query || $query->query_vars['post_type'] != 'sfhiv_group') return;
 	sfhiv_draw_taxonomy_query_menu('sfhiv_year',$query,array(
 		'title_li' => false,
-		'extra_classes' => 'filters',
+		'extra_classes' => 'filter filter-horizontal',
 		'base_link' => get_permalink(),
 	));
 }
