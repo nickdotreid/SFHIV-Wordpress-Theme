b7af2c8d054f89cc2c05e4e3297b424aa3c00b0d
Nick Reid <nickreid@nickreid.com>
2012-06-01 16:32:03 -0700

    removed all link to page functions

--- include/common-page.php
+++ include/common-page.php
@@ -19,16 +19,6 @@ function sfhiv_page_show_website_link(){
 	<?
 }
 
-add_action('get_sidebar','sfhiv_page_show_linked_page',11);
-function sfhiv_page_show_linked_page(){
-	if(!is_singular()) return;
-	$page = sfhiv_link_to_page_get_page(get_the_ID());
-	if(!$page) return;
-	sfhiv_draw_menu(array($page),array(
-		"show_parents" => true,
-	));
-}
-
 add_action('after_content','sfhiv_page_list_attachments',5);
 function sfhiv_page_list_attachments(){
 	if(!is_singular()) return;
