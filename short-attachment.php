cf694549da7519c3435c69363433e3a5f6e09bd9
Nick Reid <nickreid@nickreid.com>
2012-06-18 12:03:12 -0700

    styled attachments

--- short-attachment.php
+++ short-attachment.php
@@ -1,12 +1,12 @@
 <article id="post-<?=the_ID();?>" <?php post_class("list-item mime-".sanitize_title(get_post_mime_type())); ?>>
-	<header>
-		<h3 class="entry-title"><a href="<?=wp_get_attachment_url();?>"><?=the_title();?></a></h3>
-	</header>
+	<h3 class="entry-title"><a href="<?=wp_get_attachment_url();?>"><i></i><?=the_title();?></a></h3>
+	<span class="description">
+		<span><?=get_post_mime_type();?></span>
+	</span>
+	<?	if(get_the_content() != ""):	?>
 	<div class="entry-content">
-		<?=the_excerpt();?>
+		<?=the_content();?>
 	</div>
-	<nav class="entry-navigation">
-		<a href="<?=wp_get_attachment_url();?>">Download <?=the_title();?></a>
-		<br class="clear" />
-	</nav>
+	<?	endif;	?>
+	<div class="clear"></div>
 </article>
