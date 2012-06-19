06ab3ab9358c10fc89d2c7be30e319b835550d04
Nick Reid <nickreid@nickreid.com>
2012-06-19 12:21:28 -0700

    fixed url bug

--- include/common-archive.php
+++ include/common-archive.php
@@ -3,7 +3,7 @@
 add_action('sfhiv_pre_loop','sfhiv_archive_show_pages',2,2);
 add_action('sfhiv_post_loop','sfhiv_archive_show_pages',10,2);
 function sfhiv_archive_show_pages($query=false,$args=array()){
-	global $wp_rewrite, $wp_query;
+	global $wp_rewrite, $wp_query, $_SERVER;
 	
 	if(!$query){
 		$query = $wp_query;
@@ -15,8 +15,10 @@ function sfhiv_archive_show_pages($query=false,$args=array()){
 	if($current_page < 1){
 		$current_page = 1;
 	}
+	
+	$base_url = preg_replace('/page\/(\d+)\//i','',$_SERVER[REQUEST_URI]); // strip out page var if in url
 	$args = array(
-		'base' => @add_query_arg('page','%#%'),
+		'base' => @add_query_arg('page','%#%',$base_url),
 		'total'        => $query->max_num_pages,
 		'current'      => $current_page,
 	);
@@ -26,6 +28,7 @@ function sfhiv_archive_show_pages($query=false,$args=array()){
 
 	if( !empty($query->query_vars['s']) )
 		$pagination['add_args'] = array( 's' => get_query_var( 's' ) );
+
 	echo '<section class="info-query">';
 	echo '<span class="results">';
 	_e("Showing ".$query->post_count." of ".$query->found_posts.' results');
@@ -40,7 +43,12 @@ function sfhiv_archive_show_pages($query=false,$args=array()){
 
 add_action( 'pre_get_posts', 'sfhiv_archive_page_query' );
 function sfhiv_archive_page_query( $query ) {
+	global $wp_query;
 	if(is_admin()) return;
+	if($wp_query->query_vars['paged']  && (!isset($query->query_vars['posts_per_page']) || $query->query_vars['posts_per_page'] >= 1) ){
+		$query->query_vars['paged'] = $wp_query->query_vars['paged'];
+		return;
+	}
 	if(isset($_GET['page']) && (!isset($query->query_vars['posts_per_page']) || $query->query_vars['posts_per_page'] >= 1)){
 		$query->query_vars['paged'] = $_GET['page'];
 	}
