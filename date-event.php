86cb3102ec3bc69c0ff0572eda1f837b6243ded6
Nick Reid <nickreid@nickreid.com>
2012-08-07 13:52:31 -0700

    updated calendar styles

--- date-event.php
+++ date-event.php
@@ -7,8 +7,8 @@ $time = get_post_meta(get_the_ID(),'sfhiv_event_start',true);
 <span class="date date-calendar">
 	<?	if(date("j-F-Y",$now) == date("j-F-Y",$time)):	?>
 	<span class="today"><?_e("Today","sfhiv2012");?></span>
-	<?	elseif($now+604800 > $time && $now < $time):	?>
-	<span class="day"><?=date('l',$time);?></span>
+	<?	elseif(TRUE || $now+604800 > $time && $now < $time):	?>
+	<span class="day"><?=date('D',$now);?></span>
 	<?	endif;	?>
 	<span class="date-number"><?=date("d",$time);?></span>
 	<span class="month"><?=date("M",$time);?></span>
