93306d4a91a1d6c26b0317a793ec904f007db84b
Nick Reid <nickreid@nickreid.com>
2012-08-21 08:18:50 -0700

    fixed homepage image size to not crop

--- functions-images.php
+++ functions-images.php
@@ -6,7 +6,7 @@ if ( function_exists( 'add_theme_support' ) ) {
 
 if ( function_exists( 'add_image_size' ) ) { 
 	add_image_size( 'featured-size', 450, 9999 ); //300 pixels wide (and unlimited height)
-	add_image_size( 'homepage-size', 9999, 400, true ); //(cropped)
+	add_image_size( 'homepage-size', 9999, 400 );
 	add_image_size( 'sidebar', 200, 9999);
 	
 	add_image_size( 'thumbnail', 100, 100, true);
