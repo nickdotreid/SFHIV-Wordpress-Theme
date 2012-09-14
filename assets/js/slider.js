b023f31cf2a21d790679b3da09a97cf73d982c8b
Nick Reid <nickreid@nickreid.com>
2012-09-14 14:40:00 -0700

    styling main slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-	$('.slider').addClass("carousel").addClass("slide").wrapInner("carousel-inner");
+	$('.slider').addClass("carousel").addClass("slide").wrapInner('<div class="carousel-inner">')
+		.append('<a class="carousel-control left" href="#" data-slide="prev">&lsaquo;</a>')
+		.append('<a class="carousel-control right" href="#" data-slide="next">&rsaquo;</a>');
 	$('.slider').carousel({
 		interval:5000
+	});
+	$(".slider").bind("slid",function(event){
+		$(".item.active",$(this)).fadeIn();
 	});
 });
