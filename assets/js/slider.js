7a947f4fa46cf0dafc1d5c51699894fa5615a1db
Nick Reid <nickreid@nickreid.com>
2012-09-19 17:43:01 -0700

    added fade effect to carousel

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -4,6 +4,11 @@ $(document).ready(function(){
 		.append('<a class="carousel-control left" href="#'+slider_id+'" data-slide="prev">&lsaquo;</a>')
 		.append('<a class="carousel-control right" href="#'+slider_id+'" data-slide="next">&rsaquo;</a>');
 	$(".carousel .item:first").addClass("active");
+	$('.slider').bind("slide",function(event){
+		$('.item.active',$(this)).fadeOut('slow');
+	}).bind("slid",function(event){
+		$('.item.active',$(this)).fadeIn('slow');
+	});
 	$('.slider').carousel({
 		interval:5000
 	});
