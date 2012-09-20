81b326fe89f497c09587b31dce9169a3ff1e115d
Nick Reid <nickreid@nickreid.com>
2012-09-19 17:09:11 -0700

    added transition and made first slide more obvious

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -3,10 +3,8 @@ $(document).ready(function(){
 	$('.slider').attr("id",slider_id).addClass("carousel").addClass("slide").wrapInner('<div class="carousel-inner">')
 		.append('<a class="carousel-control left" href="#'+slider_id+'" data-slide="prev">&lsaquo;</a>')
 		.append('<a class="carousel-control right" href="#'+slider_id+'" data-slide="next">&rsaquo;</a>');
+	$(".carousel .item:first").addClass("active");
 	$('.slider').carousel({
 		interval:5000
-	});
-	$(".slider").bind("slid",function(event){
-		$(".item.active",$(this)).fadeIn();
 	});
 });
