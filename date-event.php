1c5f70ac2605b0b38a25bd8beddc037608b82922
Nick Reid <nickreid@nickreid.com>
2012-10-15 12:58:13 -0700

    made date not use today for day value

--- date-event.php
+++ date-event.php
@@ -8,7 +8,7 @@ $time = get_post_meta(get_the_ID(),'sfhiv_event_start',true);
 	<?	if(date("j-F-Y",$now) == date("j-F-Y",$time)):	?>
 	<span class="today"><?_e("Today","sfhiv2012");?></span>
 	<?	elseif(TRUE || $now+604800 > $time && $now < $time):	?>
-	<span class="day"><?=date('D',$now);?></span>
+	<span class="day"><?=date('D',$time);?></span>
 	<?	endif;	?>
 	<span class="date-number"><?=date("d",$time);?></span>
 	<span class="month"><?=date("M",$time);?></span>
