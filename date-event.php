43d36856921982be8e527fb40686dc8d6761bd3e
Nick Reid <nickreid@nickreid.com>
2012-10-15 18:06:40 -0700

    changed to full day representation

--- date-event.php
+++ date-event.php
@@ -8,7 +8,7 @@ $time = get_post_meta(get_the_ID(),'sfhiv_event_start',true);
 	<?	if(date("j-F-Y",$now) == date("j-F-Y",$time)):	?>
 	<span class="day today"><?_e("Today","sfhiv2012");?></span>
 	<?	elseif($now+604800 > $time && $now < $time):	?>
-	<span class="day"><?=date('D',$time);?></span>
+	<span class="day"><?=date('l',$time);?></span>
 	<?	endif;	?>
 	<span><?=date("F d, Y",$time);?></span>
 </div>
