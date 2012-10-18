244b65e8567a2659f11680b4bcf2eda40c549993
Nick Reid <nickreid@nickreid.com>
2012-10-18 14:03:11 -0700

    made table of contents parse content more carefully and look for ids, not names

--- include/table_of_contents.php
+++ include/table_of_contents.php
@@ -7,13 +7,15 @@ function sfhiv_extract_toc($html_string,$depth=3){
 	$links = array();
 	foreach($winners[0] as $link){
 		$_link = array();
-		$pieces = explode('name="',$link);
+		$pieces = explode('id="',$link);
 		$pieces = explode('"',$pieces[1]);
 		$_link['anchor'] =  $pieces[0];
 		$pieces = explode(">",$pieces[count($pieces)-1]);
 		$pieces = explode("<",$pieces[1]);
 		$_link['title'] =  $pieces[0];
-		$links[] = $_link;
+		if($_link['anchor'] != "" && $_link['title'] != ""){
+			$links[] = $_link;
+		}
 	}
 	return $links;
 }
