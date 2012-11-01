3dfe2990b6f1da250a98e5a501cd17bd34122742
Nick Reid <nickreid@nickreid.com>
2012-11-01 16:50:19 -0700

    made service times listing into function

--- short-sfhiv_provider.php
+++ short-sfhiv_provider.php
@@ -14,32 +14,7 @@
 			<div class="entry-content">
 				<?=apply_filters("content",$service->post_content);?>
 			</div>
-			<ul class="list sfhiv_service_time_hour service-time-list">
-			<?
-			$locations = sfhiv_service_hours_sort_by_location($service->times);
-			foreach($locations as $location){
-				echo '<li class="sfhiv_service_hour type-sfhiv_service_hour status-publish hentry list-item">';
-				$days = sfhiv_service_hours_sort_by_day($location->times);
-				echo '<div class="times-list">';
-				foreach($days as $day){
-					echo '<div class="time-container">';
-					echo '<div class="date date-float">';
-					sfhiv_service_hour_markup_day($day['day']);
-					echo '</div>';
-					foreach($day['times'] as $time){
-						sfhiv_service_hour_display_time_markup($time->start,$time->end,$time_format);	
-					}
-					echo '<div class="clear">&nbsp;</div>';
-					echo '</div><!-- end .time-container -->';
-				}
-				echo '</div><!-- end .times-list -->';
-				sfhiv_location_format($location);
-				echo '<div class="clear">&nbsp;</div>';
-				echo '</li>';
-			}
-			?>
-			</ul>
-			<div class="clear">&nbsp;</div>
+			<?	sfhiv_service_hours_print_list($service->times); ?>
 		</div>
 	<?	endforeach; ?>
 	</div>
