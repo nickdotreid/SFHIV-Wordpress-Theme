b611538a1b07e70f94b6828d0b09ed0f813e2946
Nick Reid <nickreid@nickreid.com>
2012-12-04 16:45:42 -0800

    got rid of special template for sfhiv_event

--- content-sfhiv_event.php
+++
@@ -1,9 +0,0 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<? do_action('before_content');	?>
-	<div class="entry-content">
-		<?php the_content(); ?>
-	</div><!-- .entry-content -->
-	<? do_action('after_content');	?>
-	<footer class="entry-meta">
-	</footer><!-- .entry-meta -->
-</article><!-- #post-<?php the_ID(); ?> -->
