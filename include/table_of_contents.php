2f426380ae89b825bebad83d72a1d157a60533d0
Nick Reid <nickreid@nickreid.com>
2012-12-13 14:40:52 -0800

    fixed up error

--- include/table_of_contents.php
+++ include/table_of_contents.php
@@ -8,13 +8,15 @@ function sfhiv_extract_toc($html_string,$depth=3){
 	foreach($winners[0] as $link){
 		$_link = array();
 		$pieces = explode('id="',$link);
-		$pieces = explode('"',$pieces[1]);
-		$_link['anchor'] =  $pieces[0];
-		$pieces = explode(">",$pieces[count($pieces)-1]);
-		$pieces = explode("<",$pieces[1]);
-		$_link['title'] =  $pieces[0];
-		if($_link['anchor'] != "" && $_link['title'] != ""){
-			$links[] = $_link;
+		if(count($pieces)>1){
+			$pieces = explode('"',$pieces[1]);
+			$_link['anchor'] =  $pieces[0];
+			$pieces = explode(">",$pieces[count($pieces)-1]);
+			$pieces = explode("<",$pieces[1]);
+			$_link['title'] =  $pieces[0];
+			if($_link['anchor'] != "" && $_link['title'] != ""){
+				$links[] = $_link;
+			}			
 		}
 	}
 	return $links;
