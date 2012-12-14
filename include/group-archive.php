745b6965a5329ad2ed3baf0d8a477ca9b408d6aa
Nick Reid <nickreid@nickreid.com>
2012-12-13 16:06:22 -0800

    wrapped request uri

--- include/group-archive.php
+++ include/group-archive.php
@@ -4,7 +4,7 @@ add_action('sfhiv_pre_loop','sfhiv_group_archive_filter_by_years',22,2);
 function sfhiv_group_archive_filter_by_years($query,$args){
 	if(!$query || $query->query_vars['post_type'] != 'sfhiv_group') return;
 	
-	$base_url = preg_replace('/page\/(\d+)\//i','',$_SERVER[REQUEST_URI]); // strip out page var if in url
+	$base_url = preg_replace('/page\/(\d+)\//i','',$_SERVER['REQUEST_URI']); // strip out page var if in url
 	if(isset($args['id']))	$base_url .= "#".$args['id'];
 	
 	sfhiv_draw_taxonomy_query_menu('sfhiv_year',$query,array(
